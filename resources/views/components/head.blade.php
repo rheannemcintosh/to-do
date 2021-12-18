2a64739167a1a6fb68cefcd808da37006a673c87
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-12-18 23:15:16 +0000

    Create the view for the head component

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,22 @@
+<head>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Title -->
+    <title inertia>{{ config('app.name', 'To Do') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+</head>
