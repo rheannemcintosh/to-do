49ce00238673951fa43d00c04c9381464ea78503
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-12-27 22:45:17 +0000

    Create the basic navigation

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,3 +1,8 @@
 <nav class="bg-purple-900 w-full px-4">
     <h1 class="text-4xl text-white p-4">To Do</h1>
+    <ul>
+        <li class=""><a class="" href="{{ url('/') }}">Home</a></li>
+        <li class=""><a class="" href="{{ url('/epics') }}">Epics</a></li>
+        <li class=""><a class="" href="{{ url('/tasks') }}">Tasks</a></li>
+    </ul>
 </nav>
