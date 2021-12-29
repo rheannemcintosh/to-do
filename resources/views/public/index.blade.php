4de9d9b9716708a041395e0790ad5b25be88d7e2
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2021-12-29 23:53:34 +0000

    Create a basic public homepage

---
+++ resources/views/public/index.blade.php
@@ -0,0 +1,7 @@
+<x-public-layout>
+    <div class="bg-tdTeal">
+        <h1 class="flex justify-center items-center text-white text-5xl">To Do</h1>
+        <h2 class="flex justify-center items-center text-white text-3xl">The 'EXTRA' To Do List</h2>
+        <h3 class="flex justify-center items-center text-white text-xl">Coming Soon...</h3>
+    </div>
+</x-public-layout>
