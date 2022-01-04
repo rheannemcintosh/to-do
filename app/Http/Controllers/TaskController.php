a1fb51b277dd37f72c29f4d159a2bca94b089ca7
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-01-04 21:59:02 +0000

    Supply all tasks to the index view

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,7 +14,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return view('task.index');
+        $tasks = Task::all();
+
+        return view('task.index', compact('tasks'));
     }
 
     /**
