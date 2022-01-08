adc9d25dd2f246b10c5d316165aa76e81d758322
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-01-08 23:32:31 +0000

    Update the tasks table

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,53 +1,70 @@
 <x-layout>
-    <div class="p-8">
-        <h1 class="font-abril text-4xl mb-4">Tasks</h1>
-        @if(Session::has('message'))
-            <div class="py-2 px-4 rounded-lg mb-2 text-white {{ Session::get('background')}} ">
-                {{ Session::get('message')}}
+    <h1 class="font-abril text-7xl mb-6 text-tdTeal">Task Overview</h1>
+    <div class="overflow-x-auto">
+        <div class="min-w-screen min-h-screen bg-gray-100 flex justify-center bg-gray-100 font-sans overflow-hidden">
+            <div class="w-full">
+                <div class="bg-white shadow-md rounded my-6">
+                    <table class="font-poppins min-w-max w-full table-auto">
+                        <thead>
+                            <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                <th class="py-3 px-4 text-center">ID</th>
+                                <th class="py-3 px-6 text-left">Name</th>
+                                <th class="py-3 px-6 text-left">Complete</th>
+                                <th class="py-3 px-6 text-left">Priority</th>
+                                <th class="py-3 px-6 text-left">Epic</th>
+                                <th class="py-3 px-6 text-left">Category</th>
+                                <th class="py-3 px-6 text-left">Sub-Category</th>
+                                <th class="py-3 px-6 text-left">Due Date</th>
+                                <th class="py-3 px-6 text-left">Started</th>
+                                <th class="py-3 px-6 text-left">Completed</th>
+                                <th class="py-3 px-6 text-left">Status</th>
+                                <th class="py-3 px-6 text-left">Settings</th>
+                            </tr>
+                        </thead>
+                        <tbody class="text-gray-600 text-sm font-light">
+                        @foreach($tasks as $task)
+                            <tr class="border-b border-gray-200 hover:bg-gray-100">
+                                <td class="py-3 px-4 text-center">{{ $task->id }}</td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">{{ $task->task }}</td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">{{ $task->complete }}</td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">{{ $task->priority }}</td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">{{ $task->epic_id }}</td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">{{ $task->category_id }}</td>
+                                <td class="py-3 px-6 text-left whitespace-nowrap">{{ $task->sub_category_id }}</td>
+                                @if($task->never_ending_flag)
+                                    <td colspan="3" class="py-3 px-6 text-center whitespace-nowrap">Never Ending Task</td>
+                                @else
+                                    <td class="py-3 px-6 text-left whitespace-nowrap">@isset($task->due_date){{ $task->due_date->format('Y') }}@endisset</td>
+                                    <td class="py-3 px-6 text-left whitespace-nowrap">@isset($task->started_at){{ $task->started_at->format('Y') }}@endisset</td>
+                                    <td class="py-3 px-6 text-left whitespace-nowrap">@isset($task->completed_at){{ $task->completed_at->format('Y') }}@endisset</td>
+                                @endif
+                                <td class="py-3 px-6 text-left whitespace-nowrap">Status</td>
+                                <td class="py-3 px-6 text-center">
+                                    <div class="flex item-center justify-center">
+                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                            </svg>
+                                        </div>
+                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                            </svg>
+                                        </div>
+                                        <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        @endif
-        <div class="grid grid-cols-16 bg-gray-400 p-2 text-center font-bold">
-            <p>ID</p>
-            <p class="col-span-2">Name</p>
-            <p class="">Complete</p>
-            <p class="">Priority</p>
-            <p class="">Epic</p>
-            <p class="col-span-3">Description</p>
-            <p>Category</p>
-            <p>Sub-Category</p>
-            <p>Due Date</p>
-            <p>Started</p>
-            <p>Completed</p>
-            <p>Edit</p>
-            <p>Delete</p>
         </div>
-        <div class="font-poppins grid gap-y-4 grid-cols-16 p-2 border-2 border-gray-400 text-center flex items-center justify-center">
-            @foreach($tasks as $task)
-                <p class="p-1 font-bold">{{ $task->id }}</p>
-                <p class="p-1 col-span-2">{{ $task->task }}</p>
-                <p class="p-1 font-bold">{{ $task->complete }}</p>
-                <p class="p-1 font-bold">{{ $task->priority }}</p>
-                <p class="p-1 font-bold">{{ $task->epic_id }}</p>
-                <p class="p-1 col-span-3 text-sm text-left">{{ $task->description }}</p>
-                <p class="p-1 font-bold">{{ $task->category_id }}</p>
-                <p class="p-1 font-bold">{{ $task->sub_category_id }}</p>
-                @if($task->never_ending_flag)
-                    <div class="text-sm bg-gray-200 rounded-lg p-1 col-span-3">
-                        <p>Never Ending Task!</p>
-                    </div>
-                @else
-                    <p class="p-1 text-sm">{{ $task->due_date }}</p>
-                    <p class="p-1 text-sm">{{ $task->started_at }}</p>
-                    <p class="p-1 text-sm">{{ $task->completed_at }}</p>
-                @endif
-                <button class="h-8 px-4 m-2 mx-8 text-sm text-white bg-green-600 rounded-lg focus:shadow-outline hover:bg-green-800">Edit</button>
-                <form method="post" action="">
-                    @csrf
-                    @method('DELETE')
-                    <button class="h-8 px-4 m-2 mx-8 text-sm text-white bg-red-600 rounded-lg focus:shadow-outline hover:bg-red-800">Delete</button>
-                </form>
-            @endforeach
-        </div>
-
     </div>
 </x-layout>
