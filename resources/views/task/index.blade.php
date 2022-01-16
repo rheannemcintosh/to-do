e412e582c32a2e2e1d86a613515ea667b2b3e42c
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-01-16 23:00:46 +0000

    Delete a task with the small delete button

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
     <h1 class="font-abril text-7xl mb-6 text-tdTeal">Task Overview</h1>
+    @if(Session::has('message'))
+        <div class="py-2 px-4 rounded-lg mb-2 text-white {{ Session::get('background')}} ">
+            {{ Session::get('message')}}
+        </div>
+    @endif
     <div class="overflow-x-auto">
         <div class="min-w-screen min-h-screen bg-gray-100 flex justify-center bg-gray-100 font-sans overflow-hidden">
             <div class="w-full">
@@ -81,11 +86,17 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                             </svg>
                                         </div>
-                                        <div class="w-4 mr-2 transform hover:text-tdTeal hover:scale-110">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </div>
+                                        <form method="post" action="{{ route('tasks.destroy', $task->id) }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button>
+                                                <div class="w-4 mr-2 transform hover:text-tdTeal hover:scale-110">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                    </svg>
+                                                </div>
+                                            </button>
+                                        </form>
                                     </div>
                                 </td>
                             </tr>
