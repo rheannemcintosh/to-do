e426f04ca19775ce8cf34a0e77b43ac60b1556f2
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-01-17 22:59:40 +0000

    Update the login and register routes

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,16 +3,16 @@
 
     <div class="relative flex items-top justify-center min-h-screen bg-tdTeal dark:bg-gray-900 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 underline">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-white">
                 Dashboard
             </Link>
 
             <template v-else>
-                <Link :href="route('login')" class="text-sm text-gray-700 underline">
+                <Link :href="route('login')" class="text-sm text-white">
                     Log in
                 </Link>
 
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
+                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-white">
                     Register
                 </Link>
             </template>
