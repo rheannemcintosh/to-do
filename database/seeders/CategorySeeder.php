832a3e0d9695a8eaad7873a65b5a6025a8d26548
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2022-01-18 22:36:58 +0000

    Create and update the category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -17,47 +17,243 @@ class CategorySeeder extends Seeder
         \DB::table('categories')->delete();
 
         Category::create([
-            'name' => 'University'
+            'id' => 1,
+            'name' => 'Learning',
+            'parent_id' => null,
         ]);
 
         Category::create([
-            'name' => 'Bullet Journal'
+            'id' => 2,
+            'name' => 'CodeCademy',
+            'parent_id' => null,
         ]);
 
         Category::create([
-            'name' => 'Occasions'
+            'id' => 3,
+            'name' => 'ZTM Academy',
+            'parent_id' => 1,
         ]);
 
         Category::create([
-            'name' => 'Health'
+            'id' => 4,
+            'name' => 'Treehouse',
+            'parent_id' => 1,
         ]);
 
         Category::create([
-            'name' => 'Flat'
+            'id' => 5,
+            'name' => 'Udemy',
+            'parent_id' => 1,
         ]);
 
         Category::create([
-            'name' => 'Development'
+            'id' => 6,
+            'name' => 'Laracasts',
+            'parent_id' => 1,
         ]);
 
         Category::create([
-            'name' => 'Learning'
+            'id' => 7,
+            'name' => 'Duolingo',
+            'parent_id' => 1,
         ]);
 
         Category::create([
-            'name' => 'Money'
+            'id' => 8,
+            'name' => 'University of Bath',
+            'parent_id' => null,
         ]);
 
         Category::create([
-            'name' => 'Organisation'
+            'id' => 9,
+            'name' => 'Application',
+            'parent_id' => 8,
         ]);
 
         Category::create([
-            'name' => 'Projects'
+            'id' => 10,
+            'name' => 'Induction',
+            'parent_id' => 8,
         ]);
 
         Category::create([
-            'name' => 'Work'
+            'id' => 11,
+            'name' => 'Modules',
+            'parent_id' => 8,
+        ]);
+
+        Category::create([
+            'id' => 12,
+            'name' => 'Side Projects',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 13,
+            'name' => '365 Days',
+            'parent_id' => 12,
+        ]);
+
+        Category::create([
+            'id' => 14,
+            'name' => 'Budgeting 101',
+            'parent_id' => 12,
+        ]);
+
+        Category::create([
+            'id' => 15,
+            'name' => 'Development Projects',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 16,
+            'name' => 'To Do Application',
+            'parent_id' => 15,
+        ]);
+
+        Category::create([
+            'id' => 17,
+            'name' => 'Rheanne McIntosh Portfolio',
+            'parent_id' => 15,
+        ]);
+
+        Category::create([
+            'id' => 18,
+            'name' => 'Job Advert Analyser',
+            'parent_id' => 15,
+        ]);
+
+        Category::create([
+            'id' => 19,
+            'name' => 'German Flash Cards',
+            'parent_id' => 15,
+        ]);
+
+        Category::create([
+            'id' => 20,
+            'name' => 'Budgeting Project',
+            'parent_id' => 15,
+        ]);
+
+        Category::create([
+            'id' => 21,
+            'name' => 'Money',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 22,
+            'name' => 'Budget',
+            'parent_id' => 21,
+        ]);
+
+        Category::create([
+            'id' => 23,
+            'name' => 'Credit Score',
+            'parent_id' => 21,
+        ]);
+
+        Category::create([
+            'id' => 24,
+            'name' => 'Banking',
+            'parent_id' => 21,
+        ]);
+
+        Category::create([
+            'id' => 25,
+            'name' => 'Work',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 26,
+            'name' => 'Occasions',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 27,
+            'name' => 'Holidays',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 28,
+            'name' => 'Health',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 29,
+            'name' => 'Migraine',
+            'parent_id' => 28,
+        ]);
+
+        Category::create([
+            'id' => 30,
+            'name' => 'Heart',
+            'parent_id' => 28,
+        ]);
+
+        Category::create([
+            'id' => 31,
+            'name' => 'Organisation',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 32,
+            'name' => 'Bullet Journal',
+            'parent_id' => 31,
+        ]);
+
+        Category::create([
+            'id' => 33,
+            'name' => 'Notion',
+            'parent_id' => 31,
+        ]);
+
+        Category::create([
+            'id' => 34,
+            'name' => 'Home',
+            'parent_id' => null,
+        ]);
+
+        Category::create([
+            'id' => 35,
+            'name' => 'Moving Flat',
+            'parent_id' => 34,
+        ]);
+
+        Category::create([
+            'id' => 36,
+            'name' => 'Deep Clean',
+            'parent_id' => 34,
+        ]);
+
+        Category::create([
+            'id' => 37,
+            'name' => 'Organisation',
+            'parent_id' => 34,
+        ]);
+
+        Category::create([
+            'id' => 38,
+            'name' => 'Warranties',
+            'parent_id' => 34,
+        ]);
+
+        Category::create([
+            'id' => 39,
+            'name' => 'Decorating',
+            'parent_id' => 34,
+        ]);
+
+        Category::create([
+            'id' => 40,
+            'name' => 'Social',
+            'parent_id' => null,
         ]);
     }
 }
